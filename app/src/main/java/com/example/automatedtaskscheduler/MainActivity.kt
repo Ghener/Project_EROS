cb99583ef83ee81d4f46af2f5f31fcb01068beb1
GHIE <somozaghener123@gmail.com>
2022-04-04 15:32:53 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/automatedtaskscheduler/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.automatedtaskscheduler
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import com.example.automatedtaskscheduler.databinding.ActivityMainBinding
+
+public const val SPLASH_TIME = 4000L
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val firstTime = 1
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        )
+        Handler(Looper.myLooper()!!).postDelayed(
+            {
+                if(firstTime == 0) {
+                    val intent = Intent(this, Setup::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else {
+                    val intent = Intent(this, Home::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            },
+            SPLASH_TIME
+        )
+
+        val fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_animation)
+
+        binding.ivErosLogo.animation = fadeInAnim
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/automatedtaskscheduler/Setup.kt
@@ -0,0 +1,47 @@
+ package com.example.automatedtaskscheduler
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+ class Setup : AppCompatActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_setup)
+
+         val rank = resources.getStringArray(R.array.ranking)
+
+         val schoolRank = findViewById<View>(R.id.spinnerSchool) as Spinner
+         val familyRank = findViewById<View>(R.id.spinnerFamily) as Spinner
+         val personalRank = findViewById<View>(R.id.spinnerPersonal) as Spinner
+
+         val myAdapter = ArrayAdapter(
+             this@Setup,
+             android.R.layout.simple_list_item_1, resources.getStringArray(R.array.ranking)
+         )
+         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+         schoolRank.adapter = myAdapter
+         familyRank.adapter = myAdapter
+         personalRank.adapter = myAdapter
+
+         val schoolHours = findViewById<View>(R.id.spinnerSchoolHours) as Spinner
+         val familyHours = findViewById<View>(R.id.spinnerFamilyHours) as Spinner
+         val personalHours = findViewById<View>(R.id.spinnerPersonalHours) as Spinner
+
+         val adapterHours = ArrayAdapter(
+             this@Setup,
+             android.R.layout.simple_list_item_1, resources.getStringArray(R.array.hours)
+         )
+         adapterHours.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+         schoolHours.adapter = adapterHours
+         familyHours.adapter = adapterHours
+         personalHours.adapter = adapterHours
+
+
+     }
+ }
+
